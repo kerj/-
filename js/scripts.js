cc08ce7ee05a0e090d9ef576cf2c289b12aa73ae
Justin Kerntz <kerntzj@gmail.com>
2018-03-27 15:52:06 -0700

    Add more jquery functions and style

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,37 @@
 $(document).ready(function() {
   $("h1").click(function() {
     alert("This is a header.");
+    $('p').removeClass();
+    $('p').addClass('highlighter');
   });
   $("p").click(function() {
     alert("this is a paragraph.");
+    $('p').toggleClass('highlighter');
   });
-  $("img").click(function() {
-    alert("this is an image.");
-    alert("an image of a walrus!")
+  $(".walrus").click(function() {
+    alert("an image of a walrus!");
+  });
+  $("#fancyWalrus").click(function() {
+    alert("Fade Walrus!")
+    $('.fader').fadeToggle();
+  });
+  $('#famousWalrus').click(function() {
+    alert("Slide Walrus!");
+    $('.slide').slideToggle();
+  });
+  $('button#dark').click(function() {
+    alert("Engage Dark Theme")
+    $('body').removeClass();
+    $('body').addClass('darkTheme');
+  });
+  $('button#light').click(function() {
+    alert("Engage Light Theme")
+    $('body').removeClass();
+    $('body').addClass('lightTheme');
+  });
+  $('button#original').click(function(){
+    alert("Engage Original Theme")
+    $('body').removeClass();
+    $('body').addClass(originalTheme);
   });
 });
